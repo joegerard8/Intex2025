b3781768a8f74a02c786bb644b1f58cd9abf468d
Joe Gerard <167501805+joegerard8@users.noreply.github.com>
2025-04-07 13:45:28 -0600

    adding new item recommendation table to the db context

---
+++ backend/Models/ItemRecommendation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace backend.Models;
+
+public partial class ItemRecommendation
+{
+    public string ShowId { get; set; } = null!;
+
+    public string Movie { get; set; } = null!;
+
+    public string Recommendation1 { get; set; } = null!;
+
+    public string Recommendation2 { get; set; } = null!;
+
+    public string Recommendation3 { get; set; } = null!;
+
+    public string Recommendation4 { get; set; } = null!;
+
+    public string Recommendation5 { get; set; } = null!;
+
+    public string Recommendation6 { get; set; } = null!;
+
+    public string Recommendation7 { get; set; } = null!;
+
+    public string Recommendation8 { get; set; } = null!;
+
+    public string Recommendation9 { get; set; } = null!;
+
+    public string Recommendation10 { get; set; } = null!;
+}
