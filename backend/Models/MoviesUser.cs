30dbc282a831e5da1f3bc121178758ef999ce395
Joe Gerard <167501805+joegerard8@users.noreply.github.com>
2025-04-08 16:26:42 -0600

    commiting changes to all the models, they should all be working and not need to be updated at all anymore

--- backend/Models/MoviesUser.cs
+++ backend/Models/MoviesUser.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace backend.Models;
 
+[Table("movies_users")]
 public class MoviesUser
 {
     [Key]
     [Required]
+    [Column("user_id")]
     public byte UserId { get; set; }
 
     public string Name { get; set; } = null!;
@@ -20,20 +23,28 @@ public class MoviesUser
 
     public string Gender { get; set; } = null!;
 
+    [Column("Netflix")]
     public bool Netflix { get; set; }
 
+    [Column("Amazon_Prime")]
     public bool AmazonPrime { get; set; }
 
+    [Column("Disney")]
     public bool Disney { get; set; }
 
+    [Column("Paramount")]
     public bool Paramount { get; set; }
 
+    [Column("Max")]
     public bool Max { get; set; }
 
+    [Column("Hulu")]
     public bool Hulu { get; set; }
 
+    [Column("Apple_TV")]
     public bool AppleTv { get; set; }
 
+    [Column("Peacock")]
     public bool Peacock { get; set; }
 
     public string City { get; set; } = null!;
