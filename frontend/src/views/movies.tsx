c190af724c65735a41feaf08393982707d00b942
Joe Gerard <167501805+joegerard8@users.noreply.github.com>
2025-04-10 15:34:22 -0600

    commiting really quick

--- frontend/src/views/movies.tsx
+++ frontend/src/views/movies.tsx
@@ -114,33 +114,40 @@ const Movies: React.FC = () => {
               </div>
               {/* Removed brand name text */}
             </div>
+            
+            <div className="search-bar-wrapper">
+              <div className="search-bar">
+                <input
+                  type="text"
+                  placeholder="Find your next movie..."
+                  value={searchTerm}
+                  onChange={(e) => handleSearchChage(e.target.value)}
+                  className="search-input"
+                />
+                <button className="search-button">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <circle cx="11" cy="11" r="8" />
+                    <line x1="21" y1="21" x2="16.65" y2="16.65" />
+                  </svg>
+                </button>
+              </div>
 
-            <div className="search-container">
-              <input
-                type="text"
-                placeholder="Find your next movie..."
-                value={searchTerm}
-                onChange={(e) => handleSearchChage(e.target.value)}
-                className="search-input"
-              />
-              <button className="search-button">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <circle cx="11" cy="11" r="8"></circle>
-                  <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-                </svg>
-              </button>
-              <GenreFilter selectedGenres={selectedGenres} setSelectedGenres={setSelectedGenres} />
+              <div className="filter-button">
+                <GenreFilter selectedGenres={selectedGenres} setSelectedGenres={setSelectedGenres} />
+              </div>
             </div>
+
+
 
             {error && (
             <div className="error-message">{error}</div>
